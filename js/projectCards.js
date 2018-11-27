3c9b4a96b13c5b507ae237f6cef2bef58317d726
austin <austinbroadhead97@gmail.com>
2018-11-26 21:41:44 -0700

    added card flips and projects, images

--- js/projectCards.js
+++ js/projectCards.js
@@ -8,11 +8,19 @@ function Project(obj){
 	this.desc = obj.description;
 	this.skills = obj.skillsUsed;
 	this.languages = obj.languagesUsed;
-	this.imgSrc = `proj${obj.id}.png`;
+	this.imgSrc = `img/proj${obj.id}.png`;
 
 	this.createCard = function(){
-		let card = document.createElement('div');
+		// create card faces - front and back
+		// let cardLink = document.createElement('a');
+		// cardLink.classList.add('cardLink');
+		// cardLink.href = this.link;
+		// cardLink.target = "_blank";
+		// projectList.appendChild(cardLink);
+		let card = document.createElement('a');
 		card.classList.add('projectCard');
+		card.href = this.link;
+		card.target = "_blank";
 		projectList.appendChild(card);
 		let cardFront = document.createElement('div');
 		cardFront.classList.add('cardFace','cardFront');
@@ -21,16 +29,51 @@ function Project(obj){
 		cardBack.classList.add('cardFace','cardBack');
 		card.appendChild(cardBack);
 
-		let projName = document.createElement('p');
+		// create card title - project name
+		let projName = document.createElement('h1');
 		projName.textContent = this.name;
 		cardFront.appendChild(projName);
-		console.log(projName);
+
+		// create card image
+		let imgContainer = document.createElement('div');
+		imgContainer.classList.add('imgContainer');
+		cardFront.appendChild(imgContainer);
+		let img = document.createElement('img');
+		img.src = this.imgSrc;
+		imgContainer.appendChild(img);
+
+		// create Description on cardBack
+		let description = document.createElement('p');
+		description.classList.add('description');
+		description.textContent = this.desc;
+		cardBack.appendChild(description);
+
+		// create skills section on cardBack
+		let skillsTitle = document.createElement('h2');
+		skillsTitle.textContent = "Skills Used";
+		cardBack.appendChild(skillsTitle);
+		let skillsContain = document.createElement('ul');
+		skillsContain.classList.add('skillsContainer');
+		var skillsArray = this.skills;
+		for(var skill in skillsArray){
+			var p = document.createElement('li');
+			p.classList.add('skill');
+			p.textContent = skill+": "+skillsArray[skill];
+			skillsContain.appendChild(p);
+		}
+		cardBack.appendChild(skillsContain);
+
+		// check for languages, if so, create
+		// var languagesArray = this.languages;
+		// var langArray = JSON.stringify(languagesArray);
+		// console.log(langArray);
+		// if(languagesArray.length > 1){
+		// 	console.log(this.name+" has languages");
+		// }
+
 	};
 }
 
-// function createProjCard(proj){
-
-// }
 
 projects.forEach(elm => {
 	let newProj = new Project(elm);
@@ -38,3 +81,9 @@ projects.forEach(elm => {
 
 });
 
+document.getElementById('projectList').addEventListener("mouseover", function(e) {
+		if(e.target && e.target.matches('.projectCard')){
+		  console.log('flip');
+		  e.target.classList.toggle('flip');
+		}
+});
--- js/projects.js
+++ js/projects.js
@@ -3,7 +3,7 @@ export const projects = [
 		"id": "001",
 		"projName": "Principles of Digital Design",
 		"link": "files/DesignPrinciples3.pdf",
-		"description":"",
+		"description":"By studying the principles of design, I have learned the basics of what makes a general product more visually appealing, more useable, and more memorable. Applying these principles to web design allows us to make sites that are truly great. Web sites that utilize these principles have increased form and function, which keeps users coming back and improves traffic.",
 		"skillsUsed":{
 			1:"Design",
 			2:"Adobe InDesign",
@@ -13,5 +13,109 @@ export const projects = [
 		"languagesUsed":{
 
 		}
-	}
+	},
+	{
+		"id": "002",
+		"projName": "Mobile App Design Guide",
+		"link": "files/mobileAppDesign.pdf",
+		"description":"Creating a design guide for a mobile app was very beneficial in helping me learn how to design a mobile app that solves a problem, meets requirements, and identifies target users. I also learned that creating site maps and wireframes helps develop the information architecture and maximizes the usefulness of the app layout. Learning how to properly plan, execute, and analyze tests on a prototype has also helped me design mobile layouts that are more user friendly.",
+		"skillsUsed":{
+			1:"Design",
+			2:"Adobe InDesign",
+			3:"Information Design",
+			4:"Prototyping",
+			5:"Site Structure",
+			6:"Wireframing"
+		},
+		"languagesUsed":{
+
+		}
+	},
+	{
+		"id": "003",
+		"projName": "Portfolio Design Guide",
+		"link": "files/broadheadPortfolioDesignGuide.pdf",
+		"description":"My design guide for a fully responsive web site is a lot more in depth than the mobile guide. Creating a concept model gave me a good idea as to what I wanted my site to contain and what pages and sections would be required. After I knew what content I wanted, going through the PostIt information architecture exercise helped me better organize that content to provide a user with the most complete and fulfilling experience. I also discovered through the wireframes and surface comps that designing a responsive site takes a lot more consideration to how elements like the navigation will change with screen sizes. I found it easier to adapt my website as I went through those than it would be to have to go back in and rewrite code.",
+		"skillsUsed":{
+			1:"Adobe InDesign",
+			2:"Information Architecture",
+			3:"Designing for Personas",
+			4:"Wireframing",
+			5:"Prototyping"
+		},
+		"languagesUsed":{
+
+		}
+	},
+	{
+		"id": "004",
+		"projName": "Responsive Site",
+		"link": "http://dgm2740.austinbroadhead.com/04advancedCSS/",
+		"description":"Learning how to build responsive sites has been extremely beneficial in helping me design and develop websites for the modern world. These sites change and adapt according to the user's screen size and device to maximize usability for all devices. This includes changing navigation and eliminating hover elements on smartphones and tablets, while keeping the content and theme static and beautiful. Responsive design is very important because more people than ever are using mobile devices as their primary browser and we want to make sure they have a great experience.",
+		"skillsUsed":{
+			1:"Responsive Design",
+			2:"Web Design",
+			3:"Web Development"
+		},
+		"languagesUsed":{
+			1:"HTML",
+			2:"CSS"
+		}
+	},
+	{
+		"id": "005",
+		"projName": "Calm Mind - SCSS",
+		"link": "http://dgm2780.austinbroadhead.com/project3GSAP/",
+		"description":"SCSS has been my favorite thing I've learned so far in my web development education. This CSS preprocessor allows me to create variables and functions in my stylesheets, and better organize styles through nesting and partials. These elements help me build websites faster and easier than I ever could before. This project also includes elements available through GreenSock. GSAP is very useful in creating subtle animations that make a website seem more professional. I love adding these little details to sites in order to provide user's with an even better experience.",
+		"skillsUsed":{
+			1:"Web Design",
+			2:"Web Development",
+			3:"Development with Plugins",
+			4:"Preprocessors",
+			5:"GreenSock Animation"
+		},
+		"languagesUsed":{
+			1:"HTML",
+			2:"CSS",
+			3:"SCSS",
+			4:"JavaScript"
+		}
+	},
+	{
+		"id": "006",
+		"projName": "Summit Primary",
+		"link": "http://summitprimarycares.com/",
+		"description":"I designed and developed this website for Summit Primary Care, a local medical office. This has been a great opportunity for me to get first-hand experience working with clients to build a site that meets all their requirements. A challenge I faced with this project was making sure all the content the clients wanted was displayed effectively across all devices. I approached and solved this problem by using icons for social media links as well as phone call links on mobile devices. Content also gets reorganized across devices to provide a smooth flowing experience for all users. While working on this project, it was very important to me that I find the right balance between creating something modern and beautiful while still maintaining the maturity and professionalism that should represent a medical clinic. Acquiring professional experience as an independent web designer and developer before I'm even halfway through my degree has been amazing and I am so grateful to have had this opportunity.",
+		"skillsUsed":{
+			1:"Web Design",
+			2:"Web Development",
+			3:"Logo & Branding Design",
+			4:"Working for Clients"
+		},
+		"languagesUsed":{
+			1:"HTML",
+			2:"CSS",
+			3:"JavaScript",
+			4:"JQuery"
+		}
+	},
 ]
+
+
+
+// PROJECT LIST TEMPLATE
+// {
+	// 	"id": "001",
+	// 	"projName": "Principles of Digital Design",
+	// 	"link": "files/DesignPrinciples3.pdf",
+	// 	"description":"",
+	// 	"skillsUsed":{
+	// 		1:"Design",
+	// 		2:"Adobe InDesign",
+	// 		3:"Information Design",
+	// 		4:"Design Principles"
+	// 	},
+	// 	"languagesUsed":{
+
+	// 	}
+	// },
